c1afee5084c2cf1ac3015378f9f0080982cfb242
Orcicorn <orcicorn@gmail.com>
2016-03-28 20:33:38 -0400

    Added Operator Icons

--- includes/js/global.js
+++ includes/js/global.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     var oName = $("#opname");                                                  //HTML ID for the SPan that contains operator names
     var sPathImg = "images/operators/";                                        //Path to the Operator images (not including image name)
     var aOperators = "";                                                       //DO NOT EDIT: Populated through the loadData() function
+    var sLastOp = "";                                                          //DO NOT EDIT: Stores the name of the last picked operator
 
 /*  [l]oadData
     Loads the Operator Data from the defined sJSON variable link.
@@ -16,7 +17,7 @@ $(document).ready(function(){
     @returns    nothing                     Populates the aOperators global variable*/
     function loadData() {
         $.getJSON(sJSON, function(data) {
-            aOperators = data;
+            aOperators = shuffleArray(data);
         });
     }
 
@@ -28,15 +29,31 @@ $(document).ready(function(){
     @params     array       required        an Array/JSON of Operators to pick from
     @returns    nothing                     Updates the UI wtih a random operator. */
     function pickRandomOperator(aOperators) {
-        sCurrent = oName.html();
-        sOP = sCurrent;
-        while(sOP == sCurrent) {
+        sOP = sLastOp;
+        while(sOP == sLastOp) {
             sOP = aOperators[Math.floor(Math.random() * aOperators.length)];
         }
         oImage.attr("src", sPathImg+sOP.toLowerCase()+".jpg");
-        oName.html(sOP);
+        oName.html("<div class=\"icon " + sOP + "\"></div>" + sOP);
+        sLastOp = sOP;
     }
 
+/*  [s]huffleArray
+    Simply shuffles an array.  The primary purpose here is to ensure that our alphabetical JSON
+    data has a random order when we first load it to avoid weighting against any operators in
+    particular.  This is probably overkill.
+    Based on the "Fisher-Yates Shuffle" (Source: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm)
+    @params     array       required        The Array to be shuffled
+    @returns    array                       The same array in random order */
+    function shuffleArray(sourceArray) {
+        for (var i = 0; i < sourceArray.length - 1; i++) {
+            var ii = i + Math.floor(Math.random() * (sourceArray.length - i));
+            var temp = sourceArray[ii];
+            sourceArray[ii] = sourceArray[i];
+            sourceArray[i] = temp;
+        }
+        return sourceArray;
+    }
 
 //Binds (Attacker and Defender buttons)
     $("#btnAttack").click(function() { pickRandomOperator(aOperators['attackers']); });
